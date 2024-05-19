5cc6be862572a26231d36c8f7021be1bbe3e43e9
Nil E <nil.erdenn@gmail.com>
2024-05-19 16:16:44 +0200

    finished 15 still wip

--- src/components/SignupPage.jsx
+++ src/components/SignupPage.jsx
@@ -8,17 +8,26 @@ function SignupPage () {
     const successStyle= {
         borderColor: 'green'
     }
-    const [Validation,setValidation] = useState({});
-    function handleValidation(word) {
+    const formStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '80%',
+        margin: '0 auto',
+    }
+    const [password,setPassword] = useState({});
+    function handlePasswordValidation(word) {
         if (word.length>0) {
-            setValidation(successStyle);
+            setPassword(successStyle);
         }
         else {
-            setValidation(errorStyle);
+            setPassword(errorStyle);
         } 
     }
     const [emailValidation,setEmailValidation] = useState({});
+    const [email,setEmail] = useState("");
+
     function handleEmailValidation(word) {
+        setEmail(word);
         if (word.includes("@")) {
             setEmailValidation(successStyle);
         }
@@ -27,29 +36,34 @@ function SignupPage () {
         } 
     }
     const [Greeting,setGreeting] = useState(null);
-    function greet (citizen) {
-        if(citizen==="en") {
+    function greet (nationality) {
+        if(nationality==="en") {
             return setGreeting("Hello")
         }
-        if(citizen==="de") {
+        if(nationality==="de") {
             return setGreeting("Hallo")
         }
-        if(citizen==="fr") {
+        if(nationality==="fr") {
             return setGreeting("Bonjour")
         }
     }
     return (
-        <div>
+        <form style={formStyle}>
+        <label htmlFor="email">Email</label>
         <input type="email" onChange={(e) => {handleEmailValidation(e.target.value)}} style={emailValidation}/>
-        <input type="password" onChange={(e) => {handleValidation(e.target.value)}} style={Validation}/>
+        <label htmlFor="password">Password</label>
+        <input type="password" onChange={(e) => {handlePasswordValidation(e.target.value)}} style={password}/>
+        <label htmlFor="nationality">Nationality</label>
         <select name="nationality" onChange={(e) => {greet(e.target.value)}}>
             <option value="">Select</option>
             <option value="en">English</option>
             <option value="de">German</option>
             <option value="fr">French</option>
         </select>
+        <button style={{marginTop: '10px'}}>Sign Up</button>
         <p>{Greeting}</p>
-</div>
+        <p>Your email is {email}</p>
+</form>
     )
 }
 
