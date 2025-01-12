5c74bf38a389f9a1fc16c2418e59f8b70fedb738
Juan Manuel <j-grafi@hotmail.com>
2025-01-12 16:55:55 +0100

    14 done

--- src/components/FaceBook/FaceBook.jsx
+++ src/components/FaceBook/FaceBook.jsx
@@ -1,18 +1,32 @@
 
 import profiles from "../../data/berlin.json";
 import './FaceBook.css'
+import { useState } from 'react'
 
 
 function FaceBook() {
+    const [isOn, setIsOn] = useState(null)
+
+    const uniqueCountries = [...new Set(profiles.map((user) => user.country))];
+    const handleClick = (country)=>{
+        setIsOn(country)
+    }
    
 
     return(
         
-        <div  >
+        <div>
+        <div>
+            {uniqueCountries.map((country)=>(
+            <button onClick={() => handleClick(country)}key={country}>{country}</button>                
+            ))}
+
+
+        </div>
             
             <div>
             {profiles.map((user) =>(
-                <div className="containter5" key={`${user.firstName}-${user.lastName}`}>
+                <div className={`containter5 switch ${user.country === isOn  ? 'color' : 'off'}`}  key={`${user.firstName}-${user.lastName}`}>
                 <div>
                 <img src={user.img} alt="imagen de perfil" />
                 </div>
